40d3eddad5ac386e92165b122097736e4e8dcdca
muhammad bintang <masbin@gmail.com>
2022-03-17 13:59:57 +0700

    costumer menjadi custimer

--- models/res_partner.py
+++ models/res_partner.py
@@ -6,4 +6,4 @@ class Partner(models.Model):
     _inherit = 'res.partner'
 
     is_pegawainya = fields.Boolean(string='Pegawai' , default=False)
-    is_costumernya = fields.Boolean(string='Costumer' , default=False)
+    is_customernya = fields.Boolean(string='Customer' , default=False)
