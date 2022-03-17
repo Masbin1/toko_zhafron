ff7358a1f0152110df3e316e2dee98dfa98d8d95
muhammad bintang <masbin@gmail.com>
2022-03-17 12:08:46 +0700

    add rest partner

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from . import partner
 from . import panggung
 from . import pelaminan
 from . import kursi_pengantin
 from . import kursi_tamu
 from . import order
+from.  import res_partner
